b71f09974d83dfc4fb8cddd308bea0e7bce25e40
Tal Amsalem <100066758+tamsalem@users.noreply.github.com>
2022-03-13 20:41:31 +0200

    Update s3.tf

--- terraform/aws/s3.tf
+++ terraform/aws/s3.tf
@@ -12,9 +12,7 @@ resource "aws_s3_bucket" "data" {
     }, {
     git_commit = "d68d2897add9bc2203a5ed0632a5cdd8ff8cefb0"
     git_file   = "terraform/aws/s3.tf"
-    }, {
-    yor_trace = "71dd8438-cc87-490f-a738-b1d54c3181ac"
-  })
+    })
 }
 
 resource "aws_s3_bucket_object" "data_object" {
